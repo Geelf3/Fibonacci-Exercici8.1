dc3704a894d2cbf03618544b8df54942aed0654f
iaw47965399 <gerard.gee.2@gmail.com>
2015-09-28 11:38:24 +0200

    Exercici acabat

--- Fibonacci-Exercici8.1/src/Fibonacci.java
+++ Fibonacci-Exercici8.1/src/Fibonacci.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Fibonacci {
 
@@ -12,20 +13,34 @@ public class Fibonacci {
 		System.out.println("La posició 1 equival a: 1");
 		fibonacci.add(1);
 		
-		// 3: 
+		// 3: do a standard loop
+		System.out.println("\nAmb un bucle normal:\n ");
 		for (int i = 2; i < 25; i++) {
 			fibonacci.add(fibonacci.get(i-1) + fibonacci.get(i-2));
 			System.out.println("La posicio "+ i + " equival a: " + fibonacci.get(i) );
 		}
 		
+		// Do it using for each
+		System.out.println("\nAmb el for each:\n ");
+		for (Integer resultat : fibonacci) {
+			System.out.println("En la posició " + fibonacci.indexOf(resultat) + " hi ha " + resultat);
+		}
+        
+        // Do it using iterator
+		System.out.println("\nAmb l'Iterator:\n ");
+        Iterator<Integer> sequencia = fibonacci.iterator();
+        while (sequencia.hasNext()) {
+        	System.out.println(sequencia.next());
+        }
+		
 		// 4: Determinate the position of "2584" number.
-		System.out.println("El número 2584 es troba a la posició " + fibonacci.indexOf(2584));
+		System.out.println("\nEl número 2584 es troba a la posició " + fibonacci.indexOf(2584));
 		
 		// 5: the 4311 value exists in this arrayList?
 		if (fibonacci.indexOf(4311) != -1) {
-			System.out.println("El valor 4311 es troba a la posicio: " + fibonacci.indexOf(4311));
+			System.out.println("\nEl valor 4311 es troba a la posicio: " + fibonacci.indexOf(4311));
 		} else {
-			System.out.println("El valor 4311 no es troba a l'array.");
+			System.out.println("\nEl valor 4311 no es troba a l'array.");
 		}
 	}
 
